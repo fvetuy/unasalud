adb61ab4a5ecfd4671b8cc9ad2c3cf7d3973b12a
vice0627 <vicenzo.carro.c@gmail.com>
2023-08-22 22:24:58 -0300

    imagenes pesan menos, lazyload implementado

--- src/components/muestra-actividades.jsx
+++ src/components/muestra-actividades.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import styles from '../style';
+import LazyLoad from 'react-lazyload';
 import { actividades } from '../constants';
 
-const muestraActividades = () => {
+const MuestraActividades = () => {
   return (
-
-      <section id='muestraActividades' className="flex justify-center items-center flex-col sm:flex-row flex-wrap sm:mb-20 m-6">   
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {actividades.map((actividad) => (
-            <div key={actividad.id} className={`${styles.flexCenter} flex-1 flex items-center flex-col m-3 mt-5`}>
-              <div className="flex items-center">
-                <div className="w-full h-full"> 
+    <section id='muestraActividades' className="flex justify-center items-center flex-col sm:flex-row flex-wrap sm:mb-20 m-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4">
+        {actividades.map((actividad) => (
+          <div key={actividad.id} className={`${styles.flexCenter} flex-1 flex items-center flex-col m-3 mt-5`}>
+            <div className="flex items-center">
+              <div className="w-full h-full">
+                <LazyLoad height={200} once placeholder={<div className="placeholder" style={{ height: '200px' }} />} >
                   <img src={actividad.img} alt="Imagen de la actividad" className='rounded-md' />
-                  <h4 className={`${styles.h3text} text-black self-auto mb-3 p-5 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl`}>
-                  {actividad.id.slice(0, 20)} 
+                </LazyLoad>
+                <h4 className={`${styles.h3text} text-black self-auto mb-3 p-5 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl`}>
+                  {actividad.id.slice(0, 20)}
                 </h4>
-                </div>
               </div>
-              <p className={`${styles.ptext} font-poppins font-normal text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl leading-normal text-black self-auto`}>
-                 {actividad.texto.slice(0, 150) + "..."} <button className="text-black bg-transparent border-none"><a href={`${"actividades/" + actividad.id}`}>Más información</a></button>
-              </p>
             </div>
-          ))}
-        </div>
-      </section>
-  )
+            <p className={`${styles.ptext} font-poppins font-normal text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl leading-normal text-black self-auto`}>
+              {actividad.texto.slice(0, 150) + "..."} <button className="text-black bg-transparent border-none"><a href={`${"actividades/" + actividad.id}`}>Más información</a></button>
+            </p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default muestraActividades
+export default MuestraActividades;
--- src/constants/index.js
+++ src/constants/index.js
@@ -72,5 +72,23 @@ export const actividades = [
     icono: IconoActividad3,
     img: ImgActividad3,
     texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-  }
+  },
+  {
+    id: "ActividadA",
+    icono: IconoActividad3,
+    img: ImgActividad3,
+    texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    id: "ActividadB",
+    icono: IconoActividad3,
+    img: ImgActividad3,
+    texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    id: "ActividadC",
+    icono: IconoActividad3,
+    img: ImgActividad3,
+    texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
 ];
