be7045fbc07f2fd2961cd6884b705845e4719c1c
santiago <santiago.colonia2007@gmail.com>
2023-11-02 16:39:13 -0300

    prueba 3 de github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
+  base: 'https://Santiago1709.github.io/una-salud',
 })
