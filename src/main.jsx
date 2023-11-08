764eda3b68c6af2f61937323145c40c05887fd22
santiago <santiago.colonia2007@gmail.com>
2023-11-08 10:11:18 -0300

    REACT ROUTER ANDDAAA PUTA MADRE

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import { Inicio, Programa, Actividades, ActivityDetail, Noticias, Admin, PageNotFound } from './pages';
-
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <HashRouter>
       <Routes>
        <Route path="/" element={<App/>}>
@@ -16,8 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="actividades/:id" index element={<ActivityDetail/>}></Route>
         <Route path="noticias" index element={<Noticias/>}></Route>
         <Route path="admin" index element={<Admin/>}></Route>
+        <Route path="*" index element={<PageNotFound/>}></Route>
        </Route>
       </Routes>
     </HashRouter>
-  </React.StrictMode>
 );
